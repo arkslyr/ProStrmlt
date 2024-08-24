e4eec1ae8e3810fd1c282c0fb3d62159f687546d
arkslyr <146913269+arkslyr@users.noreply.github.com>
2024-08-24 19:14:57 +0530

    Update hate_speech_detection_app.py
    sklearn import

--- hate_speech_detection_app.py
+++ hate_speech_detection_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
